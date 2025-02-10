0432728d7b5dddc3dd8c1614087a16153ea752d1
daninichu <danielnguyen6@icloud.com>
2025-02-10 17:43:04 +0100

    copied tests

---
+++ src/test/java/inf112/skeleton/model/entities/PlayerTest.java
@@ -0,0 +1,60 @@
+package inf112.skeleton.model.entities;
+
+import com.badlogic.gdx.math.Vector2;
+import inf112.skeleton.model.entities.Player;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PlayerTest{
+    @Test
+    void testMovement(){
+        Player player = new Player(0, 0);
+        assertEquals(new Vector2(0, 0), player.pos);
+
+        player.moveRight();
+        player.update(1f/60f);
+        assertTrue(player.getX() > 0);
+        assertEquals(0, player.getY());
+
+        player.moveDown();
+        player.update(1f/60f);
+        assertTrue(player.getY() < 0);
+
+        player.moveLeft();
+        player.update(2f/60f);
+        assertTrue(player.getX() < 0);
+
+        player.moveUp();
+        player.update(2f/60f);
+        assertTrue(player.getY() > 0);
+    }
+
+    @Test
+    void testTravelDistance(){
+        Player player = new Player(0, 0);
+        player.moveRight();
+        player.update(1);
+        float distanceStraight = player.getX();
+
+        player.pos = new Vector2(0, 0);
+        player.moveLeft();
+        player.update(1);
+        assertEquals(-distanceStraight, player.getX());
+
+        player.moveDown();
+        player.update(1);
+        assertEquals(-distanceStraight, player.getY());
+
+        player.pos = new Vector2(0, 0);
+        player.moveRight();
+        player.moveUp();
+        player.update(1);
+        assertTrue(player.getX() < distanceStraight);
+        assertTrue(player.getY() < distanceStraight);
+
+        float distanceDiagonal = (float) Math.sqrt(Math.pow(player.getX(), 2) + Math.pow(player.getY(), 2));
+        assertTrue(Math.abs(distanceStraight - distanceDiagonal) < 0.01f);
+    }
+}
