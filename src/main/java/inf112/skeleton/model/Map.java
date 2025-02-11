ea634f62f696d7379a4cbd2576ab28a552bedffe
daninichu <danielnguyen6@icloud.com>
2025-02-11 13:09:13 +0100

    WIP

--- src/main/java/inf112/skeleton/model/Map.java
+++ src/main/java/inf112/skeleton/model/Map.java
@@ -1,8 +1,10 @@
 package inf112.skeleton.model;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import inf112.skeleton.controller.InputHandler;
+import inf112.skeleton.controller.MyInputProcessor;
 import inf112.skeleton.model.entities.Player;
 import inf112.skeleton.view.ViewableEntity;
 
@@ -12,12 +14,15 @@ import inf112.skeleton.view.ViewableEntity;
 public class Map {
     private Player player;
     private InputHandler inputHandler;
+//    private MyInputProcessor inputHandler;
     private TiledMap tiledMap;
 
     public Map() {
         this.tiledMap = new TmxMapLoader().load("maps/grassMap.tmx");
         this.player = new Player(0, 0);
         this.inputHandler = new InputHandler(player);
+//        this.inputHandler = new MyInputProcessor(player);
+//        Gdx.input.setInputProcessor(inputHandler);
     }
 
     public void update(float deltaTime) {
