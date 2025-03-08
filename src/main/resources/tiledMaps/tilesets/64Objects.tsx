8e658df7b081a31f2c2dddabfceddc4175db187d
daninichu <danielnguyen6@icloud.com>
2025-03-08 03:03:21 +0100

    Rigorous CollisionTest.

---
+++ src/main/resources/tiledMaps/tilesets/64Objects.tsx
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="64Objects" tilewidth="64" tileheight="64" spacing="1" tilecount="4" columns="2">
+ <image source="64Objects.png" width="130" height="130"/>
+ <tile id="0" type="Tree">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="23" y="52" width="18" height="12"/>
+  </objectgroup>
+ </tile>
+ <tile id="1" type="Sign">
+  <properties>
+   <property name="Text" value=""/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="7" y="53" width="50" height="10"/>
+  </objectgroup>
+ </tile>
+</tileset>
