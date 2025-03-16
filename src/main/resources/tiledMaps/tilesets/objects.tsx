6067cc54ca49daca003395f826dfbb05423e6478
daninichu <danielnguyen6@icloud.com>
2025-03-16 21:51:07 +0100

    FAFO

--- src/main/resources/tiledMaps/tilesets/objects.tsx
+++ src/main/resources/tiledMaps/tilesets/objects.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="objects" tilewidth="32" tileheight="32" tilecount="4" columns="2">
- <image source="objects.png" width="64" height="64"/>
+<tileset version="1.10" tiledversion="1.11.2" name="o" tilewidth="32" tileheight="32" spacing="1" tilecount="8" columns="4">
+ <image source="objects.png" width="132" height="66"/>
  <tile id="0" type="Switch">
   <properties>
    <property name="Door" type="object" value="0"/>
@@ -19,7 +19,7 @@
    <object id="2" name="Interaction" x="2" y="32" width="28" height="24"/>
   </objectgroup>
  </tile>
- <tile id="2" type="Door">
+ <tile id="4" type="Door">
   <properties>
    <property name="Exit Door" type="object" value="0"/>
    <property name="Locked" type="bool" value="false"/>
@@ -27,6 +27,14 @@
   <objectgroup draworder="index" id="2">
    <object id="3" name="Collision" x="4" y="20" width="24" height="12"/>
    <object id="4" name="Interaction" x="4" y="32" width="24" height="20"/>
+  </objectgroup>
+ </tile>
+ <tile id="5" type="Pressure Plate">
+  <properties>
+   <property name="Link" type="object" value="0"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="Interaction" x="0" y="2" width="32" height="30"/>
   </objectgroup>
  </tile>
 </tileset>
