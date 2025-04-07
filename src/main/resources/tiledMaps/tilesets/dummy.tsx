9a3e15a05603bf3eda36a3e9c2fc234991521861
daninichu <danielnguyen6@icloud.com>
2025-04-07 15:59:38 +0200

    files

---
+++ src/main/resources/tiledMaps/tilesets/dummy.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="dummy" tilewidth="16" tileheight="16" tilecount="1" columns="1">
+ <image source="../../Char_Sprites/char_idle_down_anim.gif" width="16" height="16"/>
+ <tile id="0" type="Dummy"/>
+</tileset>
---
+++ src/main/resources/tiledMaps/tilesets/phantom.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="phantom" tilewidth="16" tileheight="16" tilecount="1" columns="1">
+ <image source="../../Enemies_Sprites/Phantom_Sprites/phantom_idle_anim_left.gif" width="16" height="16"/>
+ <tile id="0" type="Phantom"/>
+</tileset>
